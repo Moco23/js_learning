0547de005b41a7982a3a8e5180b73b8134bb377f
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-16 20:16:27 +0200

    geting a document element by id  in wariable

--- predavanje_3/index.js
+++ predavanje_3/index.js
@@ -1,6 +1,9 @@
 // getting element by id
 document.getElementById("mainHeading").innerHTML = "Momcilo Nikolic";
-document.getElementById("colorText").innerHTML = "Set color on text";
-
 // set color on text
 document.getElementById("colorText").style.color = "red";
+
+// geting a document element by in wariable
+var colorText = document.getElementById("colorText");
+colorText.innerHTML = "Frontend Developer";
+colorText.style.color = "green";
