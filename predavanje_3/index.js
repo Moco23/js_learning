4ce5071279da597f8b869eec6f39e4c1d6414309
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-16 20:02:50 +0200

    styling a element with js dom

--- predavanje_3/index.js
+++ predavanje_3/index.js
@@ -1,1 +1,6 @@
+// getting element by id
 document.getElementById("mainHeading").innerHTML = "Momcilo Nikolic";
+document.getElementById("colorText").innerHTML = "Set color on text";
+
+// set color on text
+document.getElementById("colorText").style.color = "red";
