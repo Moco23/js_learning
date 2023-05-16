bbdd551e39cdb65e1558708afeece15318e52236
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-16 19:30:04 +0200

    adding a new item in the object

--- predavanje_2/js_objects.js
+++ predavanje_2/js_objects.js
@@ -13,3 +13,14 @@ console.log(itOpermaPricing["monitr"]);
 console.log(itOpermaPricing["ssd"]);
 console.log(itOpermaPricing["kuciste"]);
 console.log(itOpermaPricing["tipkovnica"]);
+
+// adding a new item to the object
+itOpermaPricing["mis"] = 40;
+// callling a new item in the object
+console.log(itOpermaPricing);
+// calling a specific items in the console
+console.log(itOpermaPricing["monitr"]);
+console.log(itOpermaPricing["ssd"]);
+console.log(itOpermaPricing["kuciste"]);
+console.log(itOpermaPricing["tipkovnica"]);
+console.log(itOpermaPricing["mis"]);
