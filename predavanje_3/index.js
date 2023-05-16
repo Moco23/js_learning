f00c5bd195187eeb576da2168b40b7a467202a93
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-16 20:54:16 +0200

    js dom getElement with className

--- predavanje_3/index.js
+++ predavanje_3/index.js
@@ -3,7 +3,10 @@ document.getElementById("mainHeading").innerHTML = "Momcilo Nikolic";
 // set color on text
 document.getElementById("colorText").style.color = "red";
 
-// geting a document element by in wariable
+// geting a document element by id in wariable
 var colorText = document.getElementById("colorText");
 colorText.innerHTML = "Frontend Developer";
 colorText.style.color = "green";
+
+document.getElementsByClassName("mainContent")[0].style.color = "red";
+document.getElementsByClassName("mainContent")[1].style.color = "green";
