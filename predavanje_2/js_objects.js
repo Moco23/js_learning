e621ef534e38727051039772914ebdc51bb7af5d
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-16 19:08:57 +0200

    working with the js objects and calling all objects in console also calling a specific object in console

---
+++ predavanje_2/js_objects.js
@@ -0,0 +1,15 @@
+//  js objects
+var itOpermaPricing = {
+  monitr: 200,
+  ssd: 50,
+  kuciste: 100,
+  tipkovnica: 70,
+};
+// calling a objects items in the console
+console.log(itOpermaPricing);
+
+// calling a specific items in the console
+console.log(itOpermaPricing["monitr"]);
+console.log(itOpermaPricing["ssd"]);
+console.log(itOpermaPricing["kuciste"]);
+console.log(itOpermaPricing["tipkovnica"]);
--- predavanje_2/vjezba_2.js
+++ predavanje_2/vjezba_2.js
@@ -1,5 +1,7 @@
+// js array
 var itOprema = ["Maticna ploca", "Ram memorija", "SSD"];
 console.log(itOprema);
 
+// change a array item
 itOprema[2] = "Kuciste";
 console.log(itOprema);
