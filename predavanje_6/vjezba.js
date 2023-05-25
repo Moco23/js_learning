7dd00b152216c8c0abb72bf0bb8752bc2ce69f0a
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-25 22:56:50 +0200

    appending array items in p tag with FOR loop

--- predavanje_6/vjezba.js
+++ predavanje_6/vjezba.js
@@ -2,5 +2,7 @@
 var itOprema = ["Ram memorija", "Maticna ploca", "graficka karta"];
 // js loop for it oprema
 for (var komponenta in itOprema) {
-  console.log(komponenta, itOprema[komponenta]);
+  document.getElementById(
+    "vjezba"
+  ).innerHTML += `<p>${itOprema[komponenta]}</p>`;
 }
