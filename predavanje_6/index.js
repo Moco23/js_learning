bfd14c9642d3b80245bc9a6c82308ecfe06410e7
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-25 20:26:30 +0200

    working with js array and js loop

--- predavanje_6/index.js
+++ predavanje_6/index.js
@@ -1,4 +1,6 @@
+// js array
 var proizvodi = ["audio", "video", "techno"];
+// js loop
 for (var proizvod in proizvodi) {
-  console.log(proizvod);
+  console.log(proizvod, proizvodi[proizvod]);
 }
---
+++ predavanje_6/vjezba.js
@@ -0,0 +1,6 @@
+// js array
+var itOprema = ["Ram memorija", "Maticna ploca", "graficka karta"];
+// js loop for it oprema
+for (var komponenta in itOprema) {
+  console.log(komponenta, itOprema[komponenta]);
+}
