b21216e1444c23fc112f1939aa77f95f56c5ac93
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-26 13:37:46 +0200

    name validation with lover and upper case

---
+++ predavanje_7/vjezba_3.js
@@ -0,0 +1,8 @@
+// prompt name validator with lover case
+var nameValidation = prompt("What is your name?");
+nameValidation = nameValidation.toLowerCase();
+if (nameValidation == "momcilo") {
+  console.log("name is corect");
+} else {
+  console.log("name is not corect");
+}
