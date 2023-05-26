6bd011ab79bb19a7eb9d57595dc4b2bdb997a5be
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-26 12:24:12 +0200

    starting work with prompt and validate with if and else

---
+++ predavanje_7/vjezba_1.js
@@ -0,0 +1,8 @@
+// js promp
+var godine = prompt("Koliko imate godina?");
+// validate js promise
+if (godine >= 18) {
+  console.log("Imate pristup stranici");
+} else {
+  console.log("Nazalost nemate pristup stranici radi nepunih 18. godina.");
+}
