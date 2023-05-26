920fae7480cef2fcb6d67fd0a809be28f0538d56
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-26 13:06:40 +0200

    name validation

---
+++ predavanje_7/vjezba_2.js
@@ -0,0 +1,7 @@
+// name validation with prompt if and else
+var checkName = prompt("What is your name?");
+if (checkName == "Momcilo") {
+  console.log("corect name");
+} else {
+  console.log("Check your name aganin");
+}
