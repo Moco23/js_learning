0ba5a020af6b7d52c593420506d81b5a600e72bd
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-05-30 20:16:20 +0200

    start using a math floor and random

--- predavanje_8/index.js
+++ predavanje_8/index.js
@@ -4,3 +4,14 @@ var proba = document
     proba = prompt("Koliko imate godina?");
     console.log(proba);
   });
+
+// start working with math random
+var pitanja = ["kako ste", "gjde zivite", "koliko godina imate"];
+
+document
+  .getElementById("nasumicnoPitanje")
+  .addEventListener("click", function () {
+    var nasumicaOdabir = Math.floor(Math.random() * 3);
+    var odgovor = prompt(pitanja[nasumicaOdabir]);
+    console.log(odgovor);
+  });
