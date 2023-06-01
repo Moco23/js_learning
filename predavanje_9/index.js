ee249391ee7ee4d3663332781a92e1a7333f9cfe
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-01 15:17:41 +0200

    styling elements with add event lisener through for loops

--- predavanje_9/index.js
+++ predavanje_9/index.js
@@ -2,5 +2,15 @@ document.getElementById("buyBtn").addEventListener("click", function () {
   var cijena = document
     .getElementById("cijenaProizvoda")
     .getAttribute("data-cijena");
-  console.log(cijena);
+
+  console.log(`Cijena ovog proizvoda je ${cijena} eura`);
 });
+
+// adding evenlt lisener on elements with class naem using a for loop
+for (var elements = 0; elements < 10; elements++) {
+  document
+    .getElementsByClassName("btnTest")
+    [elements].addEventListener("click", function () {
+      this.style.color = "red";
+    });
+}
