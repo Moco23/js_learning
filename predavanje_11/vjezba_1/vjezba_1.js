7758c8391ad4828a2ac877a1ec621b6f95b7c244
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-06 21:23:43 +0200

    optimization code

--- predavanje_11/vjezba_1/vjezba_1.js
+++ predavanje_11/vjezba_1/vjezba_1.js
@@ -1,17 +1,6 @@
 "use strict";
-
-var slike = document.getElementsByClassName("coding_img").length;
-
-for (var i = 0; i < slike; i++) {
-  document
-    .getElementsByClassName("coding_img")
-    [i].addEventListener("click", function () {
-      var skaliranje = this.style.transform;
-
-      if (skaliranje == "scale(1.5)") {
-        this.style.transform = "scale(1.0)";
-      } else {
-        this.style.transform = "scale(1.5)";
-      }
-    });
-}
+document
+  .getElementById("img_holder")
+  .addEventListener("click", function (zoom_in_out) {
+    zoom_in_out.target.classList.toggle("zoom");
+  });
