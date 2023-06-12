c2acd37bf9f4123d8642caa397484667eef04c2f
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-12 18:24:18 +0200

    appending prompt value in P element

---
+++ predavanje_12/vjezbe/vjezba!_1.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var unesi_grad = prompt("Unesite Grad");
+var p_el = document.createElement("p");
+p_el.textContent = unesi_grad;
+document.querySelector("#gradovi").appendChild(p_el);
