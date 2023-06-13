c19ea5372019ef996d112aea1ab05b768b5936f4
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-13 21:32:50 +0200

    add login success message after success login

--- predavanje_14/index.js
+++ predavanje_14/index.js
@@ -3,12 +3,19 @@ $(document).ready(function () {
   // login info
   const user_naem1 = "admin1";
   const password1 = "password1";
+  const successMessage = $("<p>").addClass("success").insertAfter(".form");
+
+  $(".success").css({
+    fontSize: "2.4rem",
+    color: "green",
+  });
 
   $("#login_btn").click(function () {
     const user_name_holder = $("#user_name").val();
     const password_holder = $("#password").val();
     if (user_name_holder == user_naem1 && password_holder == password1) {
       $(".form").hide();
+      $(".success").text(`Login success`);
     } else {
       console.log("Login failed");
     }
