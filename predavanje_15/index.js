4f0c414a32ab66f0eeeaeb528736bcb2b39b2ec7
Momcilo Nikolic <momcilo.pos@gmail.com>
2023-06-15 16:51:23 +0200

    unpack json array on display in p element of div element

--- predavanje_15/index.js
+++ predavanje_15/index.js
@@ -1,4 +1,14 @@
 "use strict";
 $(document).ready(function () {
-  $.ajax({});
+  $.ajax({
+    url: "https://reqres.in/api/users?page=2",
+    method: "GET",
+    success: function (users) {
+      for (var user in users["data"]) {
+        $("#users_data").append(`<p>
+        ${users["data"][user]["email"]}
+        </p>`);
+      }
+    },
+  });
 });
